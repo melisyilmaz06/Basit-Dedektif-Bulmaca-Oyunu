fedbacaac0b045e61b7b09da7d3dfa27d666120f
melisyilmaz06 <250408070@ostimteknik.edu.tr>
2026-04-23 19:27:09 +0300

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-# main.py — Oyunu Başlatan Giriş Noktası
-
-import tkinter as tk
-from src.core.oyun import CriminalCaseOyunu
-
-if __name__ == "__main__":
-    kok = tk.Tk()
-    CriminalCaseOyunu(kok)
-    kok.mainloop()
